8a805e9a8a139080b7b91cd900413ecc06a3ed51
Patrick Seelinger <Patrick Seelinger>
2016-04-24 18:40:15 -0400

    have 4 graphs displaying using loop need to get axes on graphs

--- public/js/bar.js
+++ public/js/bar.js
@@ -1,57 +1,57 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
+    width = 300 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
+
+var key = function(d) {
+        return d.knowledgeType;
+    }
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+var countries = ["US", "Finland", "Denmark", "UK"];
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+var svg = {};
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10, "%");
+countries.forEach(function(entry){
+ svg[entry] = d3.select("#" + entry).append("svg")
+     .attr("width", width + margin.left + margin.right)
+     .attr("height", height + margin.top + margin.bottom)
+     .attr("id", entry + "-graph")
+   .append("g")
+     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+});
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+d3.csv('domestic.csv', function(error, data){
 
-d3.csv("domestic.csv", function(error, data) {
-  if (error) throw error;
+x.domain(data.map(key));
+    xAxis = d3.svg.axis()
+        .scale(x)
+        .orient('bottom')
 
-  x.domain(data.map(function(d) { return d.knowledgeType; }));
-  y.domain([0, d3.max(data, function(d) { return d.US; })]);
+    countries.forEach(function(entry) {
+        var g = svg[entry].append('g')
+            .attr('transform', 'translate(0, ' + margin.bottom + ')')
+            .append('g')
+            .attr('class', 'axis')
+            .attr('transform', 'translate(0, ' + height + ')')
+            .call(xAxis);
 
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+        var y = d3.scale.linear()
+            .domain([d3.min(data, function(d) { return +d[entry]; }),
+                  d3.max(data, function(d) { return +d[entry]; })])
+            .range([height*0.2, height*0.8]);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
-    
+        var bars = svg[entry].selectAll('rect')
+            .data(data, key)
+            .enter();
 
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.knowledgeType); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.US); })
-      .attr("height", function(d) { return height - y(d.US); });
+        bars.append('rect')
+            .attr('class', 'bar')
+            .attr('height', function(d) { return y(+d[entry]); })
+            .attr('width', function(d) { return x.rangeBand(); })
+            .attr('x', function(d, i) { return x(d.knowledgeType); })
+            .attr('y', function(d) { return height-y(+d[entry]); })
+    });
 });
